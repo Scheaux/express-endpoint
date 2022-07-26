02fa688dd3c518b36d9a299585d1ba201cf8b823
Scheaux <125101@inbox.ru>
2022-07-26 18:16:00 +0700

    fixes

--- script.js
+++ script.js
@@ -11,30 +11,28 @@ const port = process.env.PORT || 7070;
 let posts = [];
 let nextId = 1;
 
-app.get('/posts', (ctx, next) => {
-  ctx.response.body = posts;
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.post('/posts', (ctx, next) => {
-  const { id, content } = ctx.request.body;
+app.post('/posts', (req, res) => {
+  const { id } = req.body;
 
-  if (id !== 0) {
-    posts = posts.map(o => o.id !== id ? o : { ...o, content: content });
-    ctx.response.status = 204;
+  if (id === '0') {
+    posts.push({ ...req.body, id: nextId, created: Date.now() });
+    res.sendStatus(204);
+    nextId++;
     return;
   }
 
-  posts.push({ ...ctx.request.body, id: nextId++, created: Date.now() });
-  ctx.response.status = 204;
+  posts = [...posts, req.body];
+  res.sendStatus(204);
 });
 
-app.delete('/posts/:id', (ctx, next) => {
-  const postId = Number(ctx.params.id);
-  const index = posts.findIndex(o => o.id === postId);
-  if (index !== -1) {
-    posts.splice(index, 1);
-  }
-  ctx.response.status = 204;
+app.delete('/posts/:id', (req, res) => {
+  const postId = Number(req.params.id);
+  posts = posts.filter((x) => x.id !== postId);
+  res.sendStatus(204);
 });
 
 app.listen(port);
